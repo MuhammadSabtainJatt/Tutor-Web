6d2e399a18ad62052393bdd2c5a4a0ab6247bacd
Muhammad Sabtain <subtainjutt26@gmail.com>
2024-10-09 21:41:44 +0500

    code updated

--- src/Pages/Registeration/registertutor.jsx
+++ src/Pages/Registeration/registertutor.jsx
@@ -4,7 +4,7 @@ export default function registertutor() {
   return (
     <>
     <div className='bg-gray-600 text-gray-300 flex justify-center align-center ' style={{minHeight:"50vh"}}>
-        <h1 className='text-xl'>Our Development team is working on this page </h1>
+        <h1 className='text-xl'>This page in not developed yet </h1>
         <p>Please Contact the Developement team <br /> to see the content of this page</p>
     </div>
     </>
